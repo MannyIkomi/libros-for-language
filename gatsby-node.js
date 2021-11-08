7ec7d235271a8ab9ae0120a66c8792cc766ddb75
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-08 00:55:00 -0500

    add categories and category types to page list

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
 const path = require(`path`);
+function slugify(string) {
+  return string.replace('_', '-').replace(' ', '-').toLowerCase();
+}
 // Log out information after a build is done
 exports.onPostBuild = ({ reporter }) => {
   reporter.info(`Your Gatsby site has been built!`);
@@ -7,6 +10,8 @@ exports.onPostBuild = ({ reporter }) => {
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const BookTemplate = path.resolve(`src/templates/Book.jsx`);
+  const TagTemplate = path.resolve(`src/templates/Tag.jsx`);
+  const CategoryTemplate = path.resolve(`src/templates/Category.jsx`);
   const result = await graphql(`
     query {
       allGraphCmsBook {
@@ -15,15 +20,73 @@ exports.createPages = async ({ graphql, actions }) => {
           slug
         }
       }
+      categoryTypes: __type(name: "GraphCMS_CategoryType") {
+        enumValues {
+          name
+        }
+      }
+      allGraphCmsTopic {
+        nodes {
+          title
+          slug
+        }
+      }
+      allGraphCmsCategory {
+        nodes {
+          title
+          slug
+          categoryType
+        }
+      }
     }
   `);
+
   result.data.allGraphCmsBook.nodes.forEach((book) => {
     createPage({
       path: `/books/${book.slug}`,
       component: BookTemplate,
       context: {
-        title: book.title,
+        title: book.bookTitle,
         slug: book.slug,
+      },
+    });
+  });
+
+  result.data.allGraphCmsTopic.nodes.forEach((topic) => {
+    createPage({
+      path: `/topics/${topic.slug}`,
+      component: TagTemplate,
+      context: {
+        title: topic.title,
+        slug: topic.slug,
+      },
+    });
+  });
+
+  result.data.categoryTypes.enumValues.forEach((typeEnum) => {
+    const typeSlug = slugify(typeEnum.name);
+    // const viewName = type.name.replace('_', '-');
+    createPage({
+      path: `/categories/${typeSlug}`,
+      component: CategoryTemplate,
+      context: {
+        title: typeEnum.name.replace('_', ' '),
+        slug: typeSlug,
+        type: typeEnum.name,
+      },
+    });
+  });
+
+  result.data.allGraphCmsCategory.nodes.forEach((category) => {
+    const typeSlug = slugify(category.categoryType);
+    // const viewName = type.name.replace('_', '-');
+    createPage({
+      path: `/categories/${typeSlug}/${category.slug}`,
+      component: TagTemplate,
+      context: {
+        title: category.title,
+        slug: category.slug,
+        type: category.categoryType,
       },
     });
   });
