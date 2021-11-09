77d222ee96f0678029b17f097e170c9cfe581c8a
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-08 23:28:17 -0500

    add styles for max read/view widths

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -6,3 +6,16 @@ export const s2 = '2rem';
 export const s3 = '3rem';
 export const s4 = '4rem';
 export const s5 = '5rem';
+
+export const base4 = s0125;
+export const base16 = s1;
+
+export const MAX_TEXT_LENGTH = '72ch';
+export const MAX_VIEW_LENGTH = '80rem'; // about 1280px base 16px
+
+export const maxTypeWidth = {
+  maxWidth: MAX_TEXT_LENGTH,
+};
+export const maxViewWidth = {
+  maxWidth: MAX_VIEW_LENGTH,
+};
