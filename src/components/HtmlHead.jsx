536a62b6a44e2ba1d64d3bbd1916bf28d7f3e492
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-14 00:01:35 -0500

    remove webflow junk

--- src/components/HtmlHead.jsx
+++ src/components/HtmlHead.jsx
@@ -35,18 +35,6 @@ const HtmlHead = () => {
       <meta name="twitter:image" content={''} />
       <meta name="twitter:image:alt" content={''} />
       <meta name="twitter:card" content="summary_large_image" />
-
-      {/* from Webflow */}
-      {/* <script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-        crossorigin="anonymous"
-      ></script>
-
-      <script
-        type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
-      ></script> */}
     </Helmet>
   );
 };
