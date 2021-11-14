fed953be3f61b29a91f2fd3537b430c0444721bf
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-14 18:09:56 -0500

    Create List.jsx

---
+++ src/components/List.jsx
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import React from 'react';
+import { Global, jsx } from '@emotion/react';
+import { flex } from '../styles';
+
+export function List(props) {
+  const { children, ordered = false } = props;
+  const LIST_TYPE = ordered ? 'ol' : 'ul';
+
+  return (
+    <LIST_TYPE {...props}>
+      {React.Children.map(children, (child) => (
+        <li>{child}</li>
+      ))}
+    </LIST_TYPE>
+  );
+}
+
+export const TagList = (props) => (
+  <List
+    css={[
+      flex('row', {
+        flexWrap: 'wrap',
+      }),
+      { listStyle: 'none' },
+    ]}
+  >
+    {props.children}
+  </List>
+);
