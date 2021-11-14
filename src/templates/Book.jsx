1beb1dbbfb76d81cd39bde31c790ec387bc7a7f2
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-14 18:12:07 -0500

    mobile responsive MVP for books

--- src/templates/Book.jsx
+++ src/templates/Book.jsx
@@ -4,9 +4,25 @@ import { jsx } from '@emotion/react';
 import { graphql } from 'gatsby';
 import { DebugData } from '../components/DebugData';
 import { Footer } from '../components/Footer';
+import { Heading } from '../components/Heading';
+import { MainMenu } from '../components/MainMenu';
+import { Section } from '../components/Section';
 import { GlobalLayout } from '../components/GlobalLayout';
+import { CategoryTag, TopicTag, TextStructureTag } from '../components/Tag';
+
 import Link from '../components/Link';
-import { s0125, s05 } from '../styles';
+import {
+  ACCENT,
+  onTabletMedia,
+  PRIMARY,
+  s0125,
+  s025,
+  s05,
+  s1,
+  s2,
+} from '../styles';
+import { List, TagList } from '../components/List';
+import { Container, TextContainer } from '../components/Container';
 
 function BookTemplate({ data }) {
   const {
@@ -14,11 +30,14 @@ function BookTemplate({ data }) {
     slug,
     publisherSummary,
     bookCover,
+    categories,
+    topics,
     featured,
     actionLabel,
     actionLink,
     translator,
     contributors,
+    representations,
     publisher,
     isbn,
     awards,
@@ -30,6 +49,12 @@ function BookTemplate({ data }) {
   // const { altDescription, url, width, height } = bookCover;
   const authors = contributors.filter((c) => c.type === 'Author');
   const illustrators = contributors.filter((a) => a.type === 'Illustrator');
+  const genres = categories.filter((c) => c.categoryType === 'Genre');
+  const grades = categories.filter((c) => c.categoryType === 'Grade');
+  const languages = categories.filter((c) => c.categoryType === 'Language');
+  const textStructure = categories.filter(
+    (c) => c.categoryType === 'Text_Structure'
+  );
 
   return (
     <GlobalLayout
@@ -38,105 +63,202 @@ function BookTemplate({ data }) {
         description: publisherSummary,
       }}
     >
-      <div>
-        <h1>{data.graphCmsBook.bookTitle}</h1>
-        <div className="hero wf-section">
-          <div className="book-page-grid container">
-            <img
-              src={bookCover.url}
-              loading="eager"
-              alt={bookCover.altDescription}
-              className="book-image full-height"
-              width={bookCover.width}
-              height={bookCover.height}
-            />
-            <div
-              id="w-node-_99a0eb45-7fae-4bf9-8d9d-f3ee27a955eb-0b286f2c"
-              className="book-info"
-            >
-              <h1 className="book-title">{bookTitle}</h1>
-              {authors && (
-                <dl css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                  <dt>by</dt>
-                  <dd>
-                    {authors.length > 1
-                      ? authors.map(({ name }) => name).join(', ')
-                      : authors[0]?.name}
-                  </dd>
-                </dl>
-              )}
-              {illustrators && (
-                <dl css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                  <dt>Illustrator:</dt>
-                  <dd>
-                    {illustrators.length > 1
-                      ? illustrators.map(({ name }) => name).join(', ')
-                      : illustrators[0]?.name}
-                  </dd>
-                </dl>
-              )}
-              {/* <div className="by-line">
-                <div className="by-line label">by</div>
-                <div className="by-line"></div>
-              </div>
-              <div className="by-line">
-                <div className="by-line label">Illustrator:</div>
-                <div className="by-line"></div>
-              </div> */}
-            </div>
-            <div
+      <MainMenu />
+      <Section>
+        <Container
+          css={[
+            {
+              display: 'grid',
+              alignSelf: 'center',
+              gridAutoColumns: '1fr',
+              gridColumnGap: '1rem',
+              gridRowGap: '2rem',
+
+              gridAutoFlow: 'column',
+              gridTemplateAreas: `
+              "book-image"
+              "book-title"
+              "book-summary"
+              "translanguaging-typology"
+              `,
+              gridTemplateColumns: '1fr',
+              gridTemplateRows:
+                'min-content min-content min-content min-content auto',
+            },
+            onTabletMedia({
+              display: '-ms-grid',
+              display: 'grid',
+
+              alignSelf: 'center',
+              gridAutoColumns: '1fr',
+              gridColumnGap: '1rem',
+              gridRowGap: '2rem',
+              gridTemplateAreas: `
+              "book-title book-title"
+              "book-image book-summary"
+              "book-image translanguaging-typology"
+              "book-image ."
+              "book-image ."`,
+
+              gridTemplateColumns: '1fr 1fr',
+
+              gridTemplateRows:
+                'min-content min-content min-content min-content auto',
+            }),
+          ]}
+        >
+          <img
+            src={bookCover.url}
+            loading="eager"
+            alt={bookCover.altDescription}
+            className="book-image full-height"
+            css={{
+              gridArea: 'book-image',
+              width: '100%',
+              height: 'auto',
+              aspectRatio: `${bookCover.width} / ${bookCover.height}`,
+            }}
+            width={bookCover.width}
+            height={bookCover.height}
+          />
+          <TextContainer
+            id="w-node-_99a0eb45-7fae-4bf9-8d9d-f3ee27a955eb-0b286f2c"
+            className="book-info"
+            css={{
+              gridArea: 'book-title',
+              h2: {
+                marginBottom: s05,
+                color: ACCENT,
+              },
+              maxWidth: 'initial',
+            }}
+          >
+            <h1 className="book-title">{bookTitle}</h1>
+            {authors.length > 0 && (
+              <dl css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                <dt>by</dt>
+                <dd>
+                  {authors.length > 1
+                    ? authors.map(({ name }) => name).join(', ')
+                    : authors[0]?.name}
+                </dd>
+              </dl>
+            )}
+
+            {illustrators > 0 && (
+              <dl css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                <dt>Illustrator:</dt>
+                <dd>
+                  {illustrators.length > 1
+                    ? illustrators.map(({ name }) => name).join(', ')
+                    : illustrators[0]?.name}
+                </dd>
+              </dl>
+            )}
+          </TextContainer>
+          {publisherSummary && (
+            <TextContainer
               id="w-node-_13bdeb9c-bc2f-92fc-08a1-a5356c9e3859-0b286f2c"
               className="publisher-summary"
+              css={{
+                gridArea: 'book-summary',
+              }}
             >
-              <h2 className="book-h2">Summary</h2>
+              <Heading level={2}>Summary</Heading>
               <p>{publisherSummary}</p>
-            </div>
-            <div
+            </TextContainer>
+          )}
+
+          {textStructure.length > 0 && (
+            <TextContainer
               id="w-node-aa56faaf-4d29-5ad8-4b7d-9de5df1602f2-0b286f2c"
               className="translanguaging"
+              css={{ gridArea: 'translanguaging-typology' }}
             >
-              <h2 className="book-h2">Translanguaging Typology</h2>
-              <div>
-                <a href="#" className="collection-tag-item w-inline-block">
-                  <div className="collection-tag"></div>
-                </a>
-                <a href="#" className="collection-tag-item w-inline-block">
-                  <div className="collection-tag"></div>
-                </a>
-              </div>
-              <div className="w-richtext">{translanguagingTypology}</div>
-            </div>
+              <Heading level={2}>Translanguaging Typology</Heading>
+              <TagList>
+                {textStructure.map((structure) => {
+                  return (
+                    <Link to={`/categories/${structure.slug}`}>
+                      <TextStructureTag>{structure.title}</TextStructureTag>
+                    </Link>
+                  );
+                })}
+              </TagList>
+              <p>{translanguagingTypology}</p>
+            </TextContainer>
+          )}
+
+          {actionLink && (
             <Link to={actionLink} className="action-button">
-              {actionLabel}
+              {actionLabel || 'Learn More'}
             </Link>
-            {/* <a
+          )}
+          {/* <a
               href="#"
               id="w-node-e1ca612b-084f-e346-7c61-be9b5fb748d7-0b286f2c"
               className="action-button"
             ></a> */}
-          </div>
-        </div>
-        {/* meta data stuff… */}
-        <section className="book-metadata wf-section">
-          <section className="container book-grid">
-            <div>
-              <div>
-                <h3 className="book-h3">Languages</h3>
-                <div className="w-dyn-list">
-                  <div role="list" className="collection-tags-list w-dyn-items">
-                    <div
-                      role="listitem"
-                      className="collection-tag-item w-dyn-item"
-                    >
-                      <div className="collection-tag category"></div>
-                    </div>
-                  </div>
-                  <div className="w-dyn-empty">
-                    <div>No items found.</div>
-                  </div>
-                </div>
-              </div>
-              <div>
+        </Container>
+      </Section>
+
+      {/* meta data stuff… */}
+      <Section css={{ alignItems: 'center' }}>
+        {/* <section className="book-metadata wf-section"> */}
+        <Container
+          css={[
+            {
+              h3: {
+                marginTop: s1,
+                marginBottom: s025,
+                color: PRIMARY,
+              },
+              display: 'grid',
+
+              justifyContent: 'stretch',
+              justifyItems: 'start',
+              alignItems: 'start',
+              alignContent: 'start',
+
+              gridAutoFlow: 'row',
+              gridAutoColumns: '1fr',
+              gridAutoRows: 'min-content',
+              gridColumnGap: s1,
+              gridRowGap: s1,
+              gridTemplateColumns: '1fr',
+              gridTemplateRows: 'min-content auto',
+            },
+            onTabletMedia({
+              gridRowGap: s2,
+              gridAutoColumns: '1fr',
+
+              gridColumnGap: '16px',
+              gridRowGap: '16px',
+
+              gridTemplateAreas: '"Area"',
+              gridTemplateColumns: '1fr 1fr',
+              gridTemplateRows: 'auto auto',
+            }),
+          ]}
+        >
+          {/* <section className="container book-grid"> */}
+
+          {languages.length > 0 && (
+            <TextContainer>
+              <Heading level={3}>Languages</Heading>
+              <TagList>
+                {languages.map((language) => {
+                  return (
+                    <Link to={`/categories/${language.slug}`}>
+                      <CategoryTag>{language.title}</CategoryTag>
+                    </Link>
+                  );
+                })}
+              </TagList>
+            </TextContainer>
+          )}
+
+          {/* <div>
                 <h3>Representation</h3>
                 <div className="w-dyn-list">
                   <div role="list" className="collection-tags-list w-dyn-items">
@@ -149,87 +271,61 @@ function BookTemplate({ data }) {
                   </div>
                   <div className="w-dyn-empty"></div>
                 </div>
-              </div>
-            </div>
-            <div>
-              <h3 className="book-h3">Topics</h3>
+              </div> */}
 
-              <div className="w-dyn-list">
-                <div role="list" className="collection-tags-list w-dyn-items">
-                  <div
-                    role="listitem"
-                    className="collection-tag-item w-dyn-item"
-                  >
-                    <div className="collection-tag topic"></div>
-                  </div>
-                </div>
-                <div className="w-dyn-empty">
-                  <div>No items found.</div>
-                </div>
-              </div>
-            </div>
+          {topics.length > 0 && (
+            <TextContainer>
+              <Heading level={3}>Topics</Heading>
+              <TagList>
+                {topics.map((topic) => {
+                  return (
+                    <Link to={`/topics/${topic.slug}`}>
+                      <TopicTag>{topic.title}</TopicTag>
+                    </Link>
+                  );
+                })}
+              </TagList>
+            </TextContainer>
+          )}
 
-            <div>
-              <dl>
-                {isbn && (
-                  <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                    <dt>ISBN:</dt>
-                    <dd>{isbn}</dd>
-                  </div>
-                )}
-                {publisher && (
-                  <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                    <dt>Publisher:</dt>
-                    <dd>{publisher}</dd>
-                  </div>
-                )}
-                {translator && (
-                  <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                    <dt>Translator:</dt>
-                    <dd>{translator}</dd>
-                  </div>
-                )}
-                {awards && (
-                  <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                    <dt>Awards:</dt>
-                    <dd>{awards.length > 1 ? awards.join(', ') : awards[0]}</dd>
-                  </div>
-                )}
-                {copyrightYear && (
-                  <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
-                    <dt>Copyright:</dt>
-                    <dd>{copyrightYear}</dd>
-                  </div>
-                )}
-              </dl>
-              {/* <ul role="list" className="w-list-unstyled">
-                <li className="book-prop-val-pair">
-                  <div className="book-property">ISBN: </div>
-                  <div className="book-value"></div>
-                </li>
-                <li className="book-prop-val-pair">
-                  <div className="book-property">Publisher: </div>
-                  <div className="book-value"></div>
-                </li>
-                <li className="book-prop-val-pair">
-                  <div className="book-property">Translator: </div>
-                  <div className="book-value"></div>
-                </li>
-                <li className="book-prop-val-pair">
-                  <div className="book-property">Awards: </div>
-                  <div className="book-value"></div>
-                </li>
-                <li className="book-prop-val-pair">
-                  <div className="book-property">Copyright: </div>
-                  <div className="book-value"></div>
-                </li>
-              </ul> */}
-            </div>
-          </section>
-        </section>
-        <DebugData>{data.graphCmsBook}</DebugData>
-        <Footer />
-      </div>
+          <TextContainer>
+            <dl>
+              {isbn && (
+                <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                  <dt>ISBN:</dt>
+                  <dd>{isbn}</dd>
+                </div>
+              )}
+              {publisher && (
+                <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                  <dt>Publisher:</dt>
+                  <dd>{publisher}</dd>
+                </div>
+              )}
+              {translator && (
+                <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                  <dt>Translator:</dt>
+                  <dd>{translator}</dd>
+                </div>
+              )}
+              {awards && (
+                <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                  <dt>Awards:</dt>
+                  <dd>{awards.length > 1 ? awards.join(', ') : awards[0]}</dd>
+                </div>
+              )}
+              {copyrightYear && (
+                <div css={{ display: 'flex', gap: s0125, marginBottom: s05 }}>
+                  <dt>Copyright:</dt>
+                  <dd>{copyrightYear}</dd>
+                </div>
+              )}
+            </dl>
+          </TextContainer>
+        </Container>
+      </Section>
+      <DebugData>{data.graphCmsBook}</DebugData>
+      {/* <Footer /> */}
     </GlobalLayout>
   );
 }
@@ -245,9 +341,14 @@ export const query = graphql`
         width
         height
       }
+      representations {
+        title
+        slug
+      }
       categories {
         title
         slug
+        categoryType
       }
       contributors {
         name
