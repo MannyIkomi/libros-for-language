1eb563efd5141a91ea96a13e27a40a00da137120
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-14 01:21:15 -0500

    Update spacing.js

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -1,6 +1,8 @@
+export const s00625 = '0.0625rem'; // 2px
 export const s0125 = '0.125rem'; // 4px
 export const s025 = '0.25rem';
 export const s05 = '0.5rem';
+export const s075 = '0.75rem';
 export const s1 = '1rem'; // 16px
 export const s2 = '2rem';
 export const s3 = '3rem';
