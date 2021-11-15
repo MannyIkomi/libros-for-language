aaf316ac2dc2ff567a0471dec20adde8a897c80b
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-15 00:30:26 -0500

    remove redundant heading styles

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,42 +1,21 @@
 /** @jsx jsx */
 import React from 'react';
 import { jsx } from '@emotion/react';
-import { h1, h2, h3, h4, h5 } from '../styles';
 
 export const Heading = ({ level = 1, children, ...props }) => {
   const Level = `h${level || 1}`;
 
   switch (level) {
     case 1:
-      return (
-        <Level css={h1} {...props}>
-          {children}
-        </Level>
-      );
+      return <Level {...props}>{children}</Level>;
     case 2:
-      return (
-        <Level css={h2} {...props}>
-          {children}
-        </Level>
-      );
+      return <Level {...props}>{children}</Level>;
     case 3:
-      return (
-        <Level css={h3} {...props}>
-          {children}
-        </Level>
-      );
+      return <Level {...props}>{children}</Level>;
     case 4:
-      return (
-        <Level css={h4} {...props}>
-          {children}
-        </Level>
-      );
+      return <Level {...props}>{children}</Level>;
     case 5:
-      return (
-        <Level css={h5} {...props}>
-          {children}
-        </Level>
-      );
+      return <Level {...props}>{children}</Level>;
 
     default:
       throw new Error(`Missing heading level 1-5, recieved: "${level}"`);
