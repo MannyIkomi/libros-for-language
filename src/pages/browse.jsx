09820c5933b929e93b2344378d6762b29476457e
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-11-28 17:43:27 -0500

    MVP browse page

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -5,14 +5,27 @@ import { jsx } from '@emotion/react';
 import {
   notoSerif,
   PRIMARY,
-  ACCENT,
+  DEPRECATED_ACCENT,
   a,
   s1,
   onTabletMedia,
   flex,
   notoMono,
+  s00625,
   s05,
   grid,
+  COMPLIMENT,
+  PRIMARY40,
+  BLACK,
+  grid12Columns,
+  s4,
+  s0125,
+  boxShadow,
+  boxShadowLg,
+  onDesktopMedia,
+  s2,
+  PRIMARY20,
+  COMPLIMENT20,
 } from '../styles';
 import { DebugData } from '../components/DebugData';
 import { Heading } from '../components/Heading';
@@ -46,6 +59,7 @@ export const MonoFontLink = (props) => (
       display: 'inline-block',
       padding: s05,
       marginBottom: s05,
+      whiteSpace: 'nowrap',
     }}
     {...props}
   >
@@ -53,21 +67,52 @@ export const MonoFontLink = (props) => (
   </Link>
 );
 
-export const CategoryList = (props) => (
-  <List css={{ listStyle: 'none' }} {...props}>
+export const TagList = (props) => (
+  <List
+    css={[
+      { listStyle: 'none' },
+      { ...flex('row', { flexWrap: 'wrap', gap: s05 }) },
+      onTabletMedia({
+        ...grid({
+          gridTemplateColumns: `repeat(auto-fit, minmax(250px, 1fr))`,
+          // gridAutoRows: `min-content`,
+        }),
+      }),
+    ]}
+    {...props}
+  >
     {props.children}
   </List>
 );
+
+export const TagGroup = (props) => {
+  const { background } = props;
+  return (
+    <section
+      css={{
+        padding: s1,
+
+        background: background || DEPRECATED_ACCENT,
+        borderRadius: `${s0125} ${s1}`,
+        ...boxShadowLg,
+      }}
+      {...props}
+    >
+      {props.children}
+    </section>
+  );
+};
 
 const BrowsePage = ({ data }) => {
   const topics = data.allGraphCmsTopic.nodes;
   const authors = data.allGraphCmsAuthor.nodes;
   const illustrators = data.allGraphCmsIllustrator.nodes;
   const categoryTypes = data.categoryNames.enumValues;
-  const categories = data.allGraphCmsCategory.nodes;
-  // const genres = data.allGraphCmsGenre.nodes;
-  // const grades = data.allGraphCmsGrade.nodes;
-  // const languages = data.allGraphCmsLanguage.nodes;
+
+  const genres = data.allGraphCmsGenre.nodes;
+  const grades = data.allGraphCmsGrade.nodes;
+  const languages = data.allGraphCmsLanguage.nodes;
+  const textStructures = data.allGraphCmsTextStructure.nodes;
 
   return (
     <>
@@ -75,114 +120,150 @@ const BrowsePage = ({ data }) => {
         <MainMenu />
         <main css={{ position: 'relative' }}>
           <Section>
-            <Heading level={1}>Browse</Heading>
             <Container>
-              <Accordion
-                allowMultipleExpanded={true}
-                preExpanded={[]}
-                css={[
-                  onTabletMedia({
-                    ...grid({
-                      gridTemplateColumns: `repeat(4, 1fr)`,
-                      width: '100%',
-                    }),
+              <Heading level={1}>Browse</Heading>
+            </Container>
+
+            <Container
+              css={[
+                {
+                  ...flex('column', {
+                    alignItems: 'stretch',
                   }),
+                  gap: s1,
+                },
+                onTabletMedia({
+                  ...grid12Columns({ gridTemplateRows: `auto` }),
+                  gridAutoFlow: `row dense`,
+                }),
+                onDesktopMedia({
+                  gap: s2,
+                }),
+              ]}
+            >
+              <TagGroup
+                background={PRIMARY20}
+                css={[
+                  { color: BLACK },
+                  onTabletMedia({ gridColumn: `1 / -1` }),
                 ]}
               >
-                {categoryTypes.length > 0 &&
-                  categoryTypes.map((type) => {
-                    return (
-                      <AccordionItem
-                        key={type.name}
-                        css={{ border: '1px solid red' }}
-                        uuid={type.name}
-                      >
-                        <AccordionItemHeading>
-                          <AccordionItemButton>
-                            <Heading level={2} css={{ color: ACCENT }}>
-                              {type.name.replace('_', ' ')}
-                            </Heading>
-                          </AccordionItemButton>
-                        </AccordionItemHeading>
-                        <AccordionItemPanel>
-                          <CategoryList>
-                            {categories
-                              .filter(
-                                (category) =>
-                                  category.categoryType === type.name
-                              )
-                              .map((category) => (
-                                <MonoFontLink
-                                  to={`/categories/${slugify(
-                                    category.categoryType
-                                  )}/${category.slug}`}
-                                >
-                                  {category.title}
-                                </MonoFontLink>
-                              ))}
-                          </CategoryList>
-                        </AccordionItemPanel>
-                      </AccordionItem>
-                    );
-                  })}
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>
-                      <Heading level={2} css={{ color: ACCENT }}>
-                        Authors
-                      </Heading>
-                    </AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <CategoryList>
-                      {authors.map((author) => (
-                        <MonoFontLink to={`/authors/${author.slug}`}>
-                          {author.name}
-                        </MonoFontLink>
-                      ))}
-                    </CategoryList>
-                  </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>
-                      <Heading level={2} css={{ color: ACCENT }}>
-                        Illustrators
-                      </Heading>
-                    </AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <CategoryList>
-                      {illustrators.map((illustrator) => (
-                        <MonoFontLink to={`/illustrators/${illustrator.slug}`}>
-                          {illustrator.name}
-                        </MonoFontLink>
-                      ))}
-                    </CategoryList>
-                  </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>
-                      <Heading level={2} css={{ color: ACCENT }}>
-                        Topics
-                      </Heading>
-                    </AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <CategoryList>
-                      {topics.map((topic) => (
-                        <MonoFontLink to={`/topics/${topic.slug}`}>
-                          {topic.title}
-                        </MonoFontLink>
-                      ))}
-                    </CategoryList>
-                  </AccordionItemPanel>
-                </AccordionItem>
-              </Accordion>
+                <Heading level={2} css={{ color: BLACK }}>
+                  Languages
+                </Heading>
+
+                <TagList>
+                  {languages.map((language) => (
+                    <MonoFontLink to={`/languages/${language.slug}`}>
+                      {language.title}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+
+              <TagGroup
+                background={PRIMARY20}
+                css={[onTabletMedia({ gridColumn: '1 / 5' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Genres
+                </Heading>
+
+                <TagList>
+                  {genres.map((genre) => (
+                    <MonoFontLink to={`/genres/${genre.slug}`}>
+                      {genre.title}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+
+              <TagGroup
+                background={PRIMARY20}
+                css={[onTabletMedia({ gridColumn: '5 / 9' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Text Structures
+                </Heading>
+
+                <TagList>
+                  {textStructures.map((structure) => (
+                    <MonoFontLink to={`/text-structures/${structure.slug}`}>
+                      {structure.title}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+
+              <TagGroup
+                background={PRIMARY20}
+                css={[onTabletMedia({ gridColumn: '9 / -1' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Grade Levels
+                </Heading>
+
+                <TagList>
+                  {grades.map((grade) => (
+                    <MonoFontLink to={`/grades/${grade.slug}`}>
+                      {grade.title}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+
+              <TagGroup
+                background={COMPLIMENT20}
+                css={[onTabletMedia({ gridColumn: '1 / -1' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Topics
+                </Heading>
+                <TagList>
+                  {topics.map((topic) => (
+                    <MonoFontLink to={`/topics/${topic.slug}`}>
+                      {topic.title}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+
+              <TagGroup
+                background={COMPLIMENT20}
+                css={[onTabletMedia({ gridColumn: '1 / 7' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Authors
+                </Heading>
+
+                <TagList>
+                  {authors.map((author) => (
+                    <MonoFontLink to={`/authors/${author.slug}`}>
+                      {author.name}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
+              <TagGroup
+                background={COMPLIMENT20}
+                css={[onTabletMedia({ gridColumn: '7 / -1' })]}
+              >
+                <Heading level={2} css={{ color: BLACK }}>
+                  Illustrators
+                </Heading>
+
+                <TagList>
+                  {illustrators.map((illustrator) => (
+                    <MonoFontLink to={`/illustrators/${illustrator.slug}`}>
+                      {illustrator.name}
+                    </MonoFontLink>
+                  ))}
+                </TagList>
+              </TagGroup>
             </Container>
           </Section>
         </main>
+
         <Footer />
       </GlobalLayout>
     </>
@@ -236,7 +317,7 @@ export const query = graphql`
         categoryType
       }
     }
-    allGraphCmsText_Structure: allGraphCmsCategory(
+    allGraphCmsTextStructure: allGraphCmsCategory(
       filter: { categoryType: { eq: Text_Structure } }
     ) {
       nodes {
--- src/styles/color.js
+++ src/styles/color.js
@@ -1,18 +1,28 @@
 // COLOR
-export const PRIMARY = '#283E5C';
+export const PRIMARY = '#283e5c';
+export const PRIMARY80 = '#4068a0';
+export const PRIMARY60 = '#659ce9';
+export const PRIMARY40 = '#9ac4fe';
+export const PRIMARY20 = '#d0e4ff';
+export const PRIMARY_WHITE = '#f0f6ff';
 // export const PRIMARY80 = '#53657d';
-export const PRIMARY66 = '#4870A8';
 // export const PRIMARY50 = '#949fae';
-export const PRIMARY33 = '#649BE8';
 // export const PRIMARY20 = '#d4d8de';
-export const COMPLIMENT = '#E2CF92';
-export const ACCENT = '#7B6D56';
+
+export const COMPLIMENT = '#6f5f46';
+export const COMPLIMENT80 = '#91855e';
+export const COMPLIMENT60 = '#E2CF92';
+export const COMPLIMENT40 = '#bfab6b';
+export const COMPLIMENT20 = '#efe6c6';
+export const COMPLIMENT_WHITE = '#fdfbf7';
+export const DEPRECATED_ACCENT = '#7B6D56';
+
 export const BLACK = '#1C2431';
 export const WHITE = '#F4F7FB';
 
 export const colors = {
   PRIMARY,
-  ACCENT,
+  DEPRECATED_ACCENT: DEPRECATED_ACCENT,
   COMPLIMENT,
   BLACK,
   WHITE,
