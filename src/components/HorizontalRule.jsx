ea1fe3f837f46f0f1f85285c2ff3a5d49fdd2075
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-12-30 16:06:18 -0500

    Create HorizontalRule.jsx

---
+++ src/components/HorizontalRule.jsx
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import React from 'react';
+
+import { jsx } from '@emotion/react';
+import { PRIMARY40, PRIMARY80 } from '../styles';
+
+export function HorizontalRule(props) {
+  return (
+    <hr css={{ borderColor: PRIMARY80, borderStyle: 'solid' }} {...props} />
+  );
+}
