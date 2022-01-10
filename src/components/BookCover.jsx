6fd97706256ecccc22d6829f791bd845ecbe0fd2
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2022-01-10 00:59:02 -0500

    add fall back placeholder to book cover image

--- src/components/BookCover.jsx
+++ src/components/BookCover.jsx
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/react';
 import Link from './Link';
 import { base320 } from '../styles';
 import { BookImage } from './BookImage';
+import BookCoverPlaceholder from '../images/PlaceholderBookCover.png';
 
 export function BookCover(props) {
   const { link, css, book, children, ...moreProps } = props;
@@ -10,7 +11,14 @@ export function BookCover(props) {
   return (
     <Link to={link?.to || `/books/${book.slug}`}>
       <article {...moreProps} css={{ maxWidth: base320 }}>
-        {book?.bookCover && <BookImage book={book} />}
+        {book?.bookCover?.url ? (
+          <BookImage book={book} />
+        ) : (
+          <img
+            src={BookCoverPlaceholder}
+            alt={`Placeholder Bookcover for ${book.title}`}
+          />
+        )}
         {children}
       </article>
     </Link>
