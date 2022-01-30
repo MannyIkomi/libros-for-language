a52a072b293d6917a56f67e218f8223108c3b0cc
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2022-01-30 00:30:11 -0500

    Update LineRule.jsx

--- src/components/LineRule.jsx
+++ src/components/LineRule.jsx
@@ -1,12 +1,12 @@
 /** @jsx jsx */
 import React from 'react';
 import { jsx } from '@emotion/react';
-import { PRIMARY, s0125 } from '../styles';
+import { PRIMARY, s0125, s025 } from '../styles';
 
 export function LineRule({ ...props }) {
   return (
     <div
-      css={{ borderTop: `solid ${s0125} ${PRIMARY}`, width: '100%' }}
+      css={{ borderTop: `solid ${s025} ${PRIMARY}`, width: '100%' }}
       {...props}
     />
   );
