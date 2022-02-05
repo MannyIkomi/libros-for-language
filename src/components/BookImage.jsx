d1c7dfedc22ce6bc49e261821f90cf07853ab17d
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2022-02-04 20:32:44 -0500

    remove

--- src/components/BookImage.jsx
+++ src/components/BookImage.jsx
@@ -20,23 +20,5 @@ export function BookImage({ book, ...props }) {
       }}
       {...props}
     />
-    // <img
-    //   src={bookCover?.url}
-    //   width={bookCover?.width}
-    //   height={bookCover?.height}
-    //   loading="eager"
-    //   alt={bookCover?.altDescription || `${book.title} Book Cover`}
-    //   className="book-image" // important for hover effect
-    //   css={{
-    //     display: 'block',
-    //     width: '100%',
-    //     height: 'auto',
-    //     aspectRatio: `${bookCover?.width} / ${bookCover?.height}`,
-
-    //     maxWidth: '20rem',
-    //     maxHeight: '20rem',
-    //   }}
-    //   {...props}
-    // />
   );
 }
